efdf0fe83a07bf1a000641aa5daaeca054f7afba
Jackson Harrison <Jackson.Harrison@stu.barren.kyschools.us>
2024-02-22 08:01:27 -0600

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Jackson Harrison on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
